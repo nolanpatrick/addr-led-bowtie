60873138ac5905843d5d4353c7a92c98db7d4c34
Nolan Adams <nolanpatrick@localhost.localdomain>
2017-03-29 22:22:56 -0400

    added neopixel example modified

---
+++ BowTie_ADDR_OrangeBlue/BowTie_ADDR_OrangeBlue.ino
@@ -0,0 +1,16 @@
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, 5, NEO_GRB + NEO_KHZ800);
+void setup() {
+  strip.begin();
+}
+void loop() {
+  chase(strip.Color(75, 25, 0)); //ORANGE
+  chase(strip.Color(0, 25, 75)); //BLUE
+}
+static void chase(uint32_t c) {
+  for (uint16_t i = 0; i < strip.numPixels() + 0; i++) {
+    strip.setPixelColor(i  , c); // Draw new pixel
+    strip.show();
+    delay(300);
+  }
+}
